e112e6c3cf1102b63f1c519fd8728e1ea4414513
notnsas <ramapanjinararendra@gmail.com>
2025-11-01 02:43:27 +0700

    Menambahkan dockerfile dan modifikasi model deployment

--- model_deployment.py
+++ model_deployment.py
@@ -6,3 +6,6 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
